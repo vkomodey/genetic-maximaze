168767b62b8a4ff32dab16a95ff96bab56948f7b
vkomodey <vkomodey@gmail.com>
2017-05-27 01:52:57 +0300

    Finish algoritm

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 from numpy import sin, abs, max
 from helpers import *
+import time
 
 # Initial params
 # l - length of binary vector in generation
-l = 10
+l = 5
 # t - iteration maximum
-tmax = 10
+tmax = 3
 # N - individuals amount in generation.
-N = 50
+N = 30
 # P_c - crossingover likelihood
 p_c = 0.3
 # P_m - mutation likelihood
@@ -19,12 +20,27 @@ p_m = 0.1
 f = lambda x: abs(sin(x))
 fitness_function = f
 
-ksi_k_prev = init_first_generation(l, N)
+start_time = time.time()
 
-first_ancestor = selection(ksi_k_prev, fitness_function, p_s, x_f)
-second_ancestor = selection(ksi_k_prev, fitness_function, p_s, x_f)
+current_generation = init_first_generation(l, N)
 
-crossed_first_ancestor, crossed_second_ancestor = crossingover(first_ancestor, second_ancestor, p_c)
+for t in range(tmax):
+    print t
+    next_generation = np.empty([N, l])
 
-mutated_first_ancestor = mutate(crossed_first_ancestor, p_m)
-mutated_second_ancestor = mutate(crossed_second_ancestor, p_m)
+    for k in range(N / 2):
+        first_ancestor = selection(current_generation, fitness_function, p_s, x_f)
+        second_ancestor = selection(current_generation, fitness_function, p_s, x_f)
+
+        crossed_first_ancestor, crossed_second_ancestor = crossingover(first_ancestor, second_ancestor, p_c)
+
+        mutated_first_ancestor = mutate(crossed_first_ancestor, p_m)
+        mutated_second_ancestor = mutate(crossed_second_ancestor, p_m)
+
+        next_generation[2*k - 1] = mutated_first_ancestor
+        next_generation[2*k] = mutated_second_ancestor
+
+    current_generation = next_generation
+
+
+print "---- %s seconds " % (time.time() - start_time)
